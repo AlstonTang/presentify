c59c1df54b1c002fbf727e37afa436cdf36564ee
Alston Tang <145715945+AlstonTang@users.noreply.github.com>
2026-03-23 14:24:20 -0700

    PWA attempt 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,68 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'GitHub_Invertocat_White.svg'],
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+				runtimeCaching: [
+					{
+						urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'google-fonts-cache',
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+							},
+							cacheableResponse: {
+								statuses: [0, 200]
+							}
+						}
+					},
+					{
+						urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'gstatic-fonts-cache',
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+							},
+							cacheableResponse: {
+								statuses: [0, 200]
+							}
+						}
+					},
+					{
+						urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'jsdelivr-cdn-cache',
+							expiration: {
+								maxEntries: 50,
+								maxAgeSeconds: 60 * 60 * 24 * 30 // <== 30 days
+							},
+							cacheableResponse: {
+								statuses: [0, 200]
+							}
+						}
+					}
+				]
+			},
+			manifest: {
+				name: 'Presentify',
+				short_name: 'Presentify',
+				description: 'Modern, minimalist presentation creation tool',
+				theme_color: '#050811',
+				background_color: '#050811',
+				display: 'standalone',
+			}
+		})
+	],
 })
